eea90e7c3d99b9da0b03a09ed64aec7d56765cb9
biancduraes <biancaeaixa@gmail.com>
2025-09-08 00:07:50 -0300

    Update atividade.java

--- atividade.java
+++ atividade.java
@@ -437,3 +437,12 @@ listar();
         boolean inQ = false;
         for (int i=0;i<line.length();i++){
             char c =
+
+//avaliação
+Esse projeto é um sistema local de tarefas, feito pra gente organizar o que precisa ser feito, o que tá em andamento e o que já terminou.
+Dá pra criar tarefas, atualizar o status e excluir quando não precisa mais. Ele salva tudo num arquivo CSV, então funciona sem precisar de banco de dados. 
+Projeto legal para treinar Java, manipulação de arquivos e HTTP.
+
+//Idéia             
+Uma ideia pra evoluir seria criar um sistema mais completo, com interface gráfica, banco de dados de verdade e login de usuário. Também podia ter filtros, relatórios e histórico de mudanças.
+Assim ficaria mais próximo de um programa real, mas sem perder os conceitos que aprendemos com essa versão simples.
